412038e506514cf6cc8ce04b14bb868f901b104e
shubhamsingh83 <akashsingh832001@gmail.com>
2024-02-15 13:49:40 +0530

    fix navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ import styles from './Hero.module.css'
 
 const Hero = () => {
   return (
-    <div>
     <div className={styles.hero}>
+    <div >
         <h1>100 Thousand Songs, ad-free</h1>
         <h1>Over thousands podcast episodes</h1>
     </div>
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import LogoImage from "../../assets/logoImage.png"
+import styles from './Logo.module.css'
 
 function Logo() {
   return (
-    <img src={LogoImage} alt='logo' width={67}/>
+    <div className={styles.logo}>
+      <img src={LogoImage} alt='logo' width={67}/>
+    </div>
+    
   )
 }
 
