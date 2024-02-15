9b78b76480bbbc31ea2fb843e8c3e0ceb3c8a48b
shubhamsingh83 <akashsingh832001@gmail.com>
2024-02-15 11:25:18 +0530

    uplaoding search bar

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import HeroImage from '../../assets/hero-image.png'
+import styles from './Hero.module.css'
+
+
+const Hero = () => {
+  return (
+    <div>
+    <div className={styles.hero}>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+    </div>
+    <div>
+        <img src={HeroImage} width={212} alt='headphones'/>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Hero;
