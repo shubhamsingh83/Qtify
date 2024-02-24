e22891480d56c452b160a2cc8ca37709ea6ccd83
shubhamsingh83 <akashsingh832001@gmail.com>
2024-02-24 11:49:32 +0530

    ok

--- src/components/Carousel/CarouselLeftNavigation.jsx
+++ src/components/Carousel/CarouselLeftNavigation.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { useSwiper } from 'swiper/react';
-import { ReactComponent as LeftArrow } from '../../assets/LeftArrow.svg';
-import styles from './Carousel.module.css'
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { useSwiper } from 'swiper/react'
+import {ReactComponent as LeftArrow} from '../../../assets/LeftArrow.svg'
+import styles from './CarouselLeftNavigation.module.css'
 
 const CarouselLeftNavigation = () => {
-  const swiper = useSwiper();
-  const [isBeginning ,setIsBeginning] = useState(swiper.isBeginning);
+    const swiper = useSwiper();
+    const [isBeginning,setIsBeginning] = useState(swiper.isBeginning)
 
-  useEffect(()=>{
-       swiper.on("slideChange",function(){
-        setIsBeginning(swiper.isBeginning);
-       })
-  },[isBeginning, swiper])
-
+    useEffect(() => {
+        swiper.on("slideChange", () => {
+            setIsBeginning(swiper.isBeginning)
+        })
+    },[swiper])
   return (
     <div className={styles.leftNavigation}>
-        {!isBeginning ? <LeftArrow onClick={()=>swiper.slidePrev()}/>:null}
+        {!isBeginning && <LeftArrow onClick={() => swiper.slidePrev()}/>}
     </div>
   )
 }
 
-export default CarouselLeftNavigation
+export default CarouselLeftNavigation;
--- src/components/Carousel/CarouselRightNavigatio.jsx
+++ src/components/Carousel/CarouselRightNavigatio.jsx
@@ -1,22 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useSwiper } from 'swiper/react';
-import { ReactComponent as RightArrow } from '../../assets/RightArrow.svg';
-import styles from './Carousel.module.css'
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { useSwiper } from 'swiper/react'
+import {ReactComponent as RightArrow} from '../../../assets/RightArrow.svg'
+import styles from './CarouselRightNavigation.module.css'
 
 const CarouselRightNavigation = () => {
-  const swiper = useSwiper();
-  const [isEnd, setIsEnd] = useState(swiper.isEnd);
+    const swiper = useSwiper();
+    const [isEnd,setIsEnd] = useState(swiper.isEnd)
 
-  useEffect(()=>{
-       swiper.on("slideChange",function(){
-        setIsEnd(swiper.isEnd);
-       })
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[isEnd])
-
+    useEffect(() => {
+        swiper.on("slideChange", () => {
+            setIsEnd(swiper.isEnd)
+        })
+    })
   return (
     <div className={styles.rightNavigation}>
-        {!isEnd?<RightArrow onClick={()=>swiper.slideNext()}/>:null}
+        {!isEnd && <RightArrow onClick={() => swiper.slideNext()}/>}
     </div>
   )
 }
